cc609421b9814c8794b38033e40130bb6c51dfcd
ChanW.Lee <cwlee815@hotmail.com>
2015-09-18 15:32:14 +0900

    get(select)메소드 추가 for forEach 람다로 수정

--- src/main/java/member/Main.java
+++ src/main/java/member/Main.java
@@ -4,29 +4,37 @@ import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Property;
 import org.hibernate.criterion.Restrictions;
 
 import util.HibernateUtil;
 
 public class Main {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
+		List<Integer> m1 = 
+				(List<Integer>) MemberImpl.get( Projections.projectionList()
+						.add(Projections.max("seq")) );
+		MemberImpl.add("abs"+m1.get(0),"information");
+		
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		
-		
-		MemberImpl.add("abs","information");
-		
 		session.beginTransaction();
-		
 		Criteria cri = session.createCriteria(Member.class);
-		cri.add(Restrictions.like("name", "test"));
-		cri.addOrder(Order.desc("seq"));
-		List<Member> m = cri.list();
-		for(Member mm: m){
-			System.out.println(mm.getSeq()+"::"+mm.getName()+"::"+mm.getInfo());
-		}
 		
+		cri.add(Restrictions.like("info", "inf", MatchMode.START)) //'inf%'
+			.add(Property.forName("name").in(new String[]{"test","abs"}))
+			.addOrder(Order.asc("seq"));
+//		cri.addOrder(Property.forName("seq").asc());
+		List<Member> m = cri.list();
+//		for(Member mm: m){
+//			System.out.println(mm.getSeq()+"\t"+mm.getName()+"  \t"+mm.getInfo());
+//	}
+		m.forEach(mm -> 
+			System.out.println(mm.getSeq()+"\t"+mm.getName()+"  \t"+mm.getInfo()) );
 		session.getTransaction().commit();
 		
 		return ;
--- src/main/java/member/MemberImpl.java
+++ src/main/java/member/MemberImpl.java
@@ -1,18 +1,40 @@
 package member;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.ProjectionList;
 
 import util.HibernateUtil;
 
 public class MemberImpl {
 
-	@Transactional
+	/**
+	 * @param name
+	 * @param info
+	 */
 	public static void add(String name, String info){
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		Member member = new Member(name,info);
-		session.save(member);        
+		session.beginTransaction();
+		session.save(member);     
+		session.getTransaction().commit();
 	}
 	
+	/**
+	 * @param ProjectionList
+	 * @return List
+	 */
+	public static List<?> get(ProjectionList proj){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Criteria crit = session.createCriteria(Member.class);
+		crit.setProjection(proj);
+		List<?> list = crit.list();
+		session.getTransaction().commit();
+		return list;
+	}
+	
+
 }
