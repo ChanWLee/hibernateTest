8b3e3a5184a73894336ba03d0c2706b1909804ba
ChanW.Lee <cwlee815@hotmail.com>
2015-09-30 18:00:43 +0900

    양방향 관련

--- src/main/java/member/Main.java
+++ src/main/java/member/Main.java
@@ -3,6 +3,8 @@ package member;
 import org.hibernate.Criteria;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.sql.JoinType;
 import org.hibernate.transform.Transformers;
 import util.HibernateUtil;
@@ -18,37 +20,37 @@ public class Main {
 		
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
-		Criteria cri = session.createCriteria(MemberInfo.class);
+		Criteria cri = session.createCriteria(Member.class);
 //		session.savseOrUpdate(arg0);
 		
-		cri.createAlias("member","member",JoinType.LEFT_OUTER_JOIN);
-//		cri.createAlias("member", "m").add(like("m.name","abs",MatchMode.ANYWHERE));
+		cri.createAlias("memberInfos","m",JoinType.LEFT_OUTER_JOIN);
+//		cri.createAlias("member", "m").add(Restrictions.like("m.name","abs",MatchMode.ANYWHERE));
 
 //		cri.createCriteria("MemberInfo","info",JoinType.LEFT_OUTER_JOIN)
 //				.add(Restrictions.eq("name", "abs"));
 
 
-		List<MemberInfo> mi = cri.list();
+		List<Member> mi = cri.list();
 		
-		mi.forEach(System.out::println);
+//		mi.forEach(System.out::println);
 
-//		for(Member mm: m)
-//			System.out.println(mm.getSeq()+"\t"+mm.getName()+"  \t"+mm.getInfo());
+		for(Member mm: mi)
+			System.out.println(mm.getSeq()+"\t"+mm.getName()+"  \t"+mm.getInfo()+"  \t"+mm.getMemberInfos());
 		
 //		mi.forEach(mm -> 
-//			System.out.println(mm.getSeq()+"\t"+mm.getInfo()+"\tmember: "+mm.getMember().getName()+"  \t"+mm.getMember().getInfo()) );
+//			System.out.println(mm.getSeq()+"\t"+mm.getInfo()+"\tmember: "+mm.getMember()..getName()+"  \t"+mm.getMember().getInfo()) );
 
+		session.getTransaction().commit();
 		//group_concat test
-		session.getTransaction().commit();
-		
-		session = HibernateUtil.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		SQLQuery qr = session.createSQLQuery("select m.seq as seq, m.name as name, group_concat(mi.info) as info from member m right join member_info mi on mi.member_seq = m.seq group by m.seq");
-		qr.setResultTransformer(Transformers.aliasToBean(Member.class));
-
-		List<Member> li = qr.list();
-		session.getTransaction().commit();
-		li.forEach(System.out::println);
+//		
+//		session = HibernateUtil.getSessionFactory().getCurrentSession();
+//		session.beginTransaction();
+//		SQLQuery qr = session.createSQLQuery("select m.seq as seq, m.name as name, group_concat(mi.info) as info from member m right join member_info mi on mi.member_seq = m.seq group by m.seq");
+//		qr.setResultTransformer(Transformers.aliasToBean(Member.class));
+//
+//		List<Member> li = qr.list();
+//		session.getTransaction().commit();
+//		li.forEach(System.out::println);
 
 		
 	}
